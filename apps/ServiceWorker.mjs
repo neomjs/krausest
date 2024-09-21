f55d856f7dff7017cd8f903bd911ecb41f78c736
tobiu <tobiasuhlig78@gmail.com>
2024-09-21 13:35:57 +0200

    moved the version of the neo root repo over & adjusted import paths

---
+++ apps/ServiceWorker.mjs
@@ -0,0 +1,31 @@
+import Neo         from '../node_modules/neo.mjs/src/Neo.mjs';
+import * as core   from '../node_modules/neo.mjs/src/core/_export.mjs';
+import ServiceBase from '../node_modules/neo.mjs/src/worker/ServiceBase.mjs';
+
+/**
+ * @class Neo.ServiceWorker
+ * @extends Neo.worker.ServiceBase
+ * @singleton
+ */
+class ServiceWorker extends ServiceBase {
+    /**
+     * @member {String} workerId='service'
+     * @protected
+     */
+    workerId = 'service'
+
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.ServiceWorker'
+         * @protected
+         */
+        className: 'Neo.ServiceWorker',
+        /**
+         * @member {Boolean} singleton=true
+         * @protected
+         */
+        singleton: true
+    }}
+}
+
+export default Neo.setupClass(ServiceWorker);
